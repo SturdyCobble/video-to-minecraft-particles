72e28fc5b87a65db899e6548b5693592f7412e26
SturdyCobble <jeawon135@gmail.com>
2023-12-09 19:49:13 +0900

    Update make_folders.py

--- lib/make_folders.py
+++ lib/make_folders.py
@@ -1,11 +1,11 @@
 import os
 import shutil
-import os
+
 def make_resourcepack(function_name, resourcepack_location):
     if os.path.exists(resourcepack_location):
         shutil.rmtree(resourcepack_location)
-    os.makedirs(resourcepack_location + "/assets/minecraft/sounds")  # make sounds dir
-    with open(resourcepack_location + "/pack.mcmeta", "w") as file:
+    os.makedirs(os.path.join(resourcepack_location,"assets","minecraft","sounds"))  # make sounds dir
+    with open(os.path.join(resourcepack_location,"pack.mcmeta"), "w") as file:
         file.write("""{\n\x20\x20"pack":\x20{\n\t\t"pack_format":\x206,\n\t\t"description":\x20"§eGive §eit §ea §eStar §6https://github.com/gamersindo1223/video-to-minecraft-particles"\n\x20\x20}\n}""")
     with open(resourcepack_location + f"/assets/minecraft/sounds.json", "w") as file:
         file.write('{\n\x20\x20"'+function_name+'_audio":\x20{\n\t"sounds":\x20['+function_name+'_audio.ogg"]\n\x20\x20}\n}')
@@ -16,6 +16,8 @@ def make_datapack(function_name, datapack_location):
         shutil.rmtree(dirpath)
     os.makedirs(dirpath + "/data/minecraft/tags/functions")
     os.makedirs(dirpath + f"/data/{function_name}/functions/src")
+    with open(os.path.join(dirpath,"pack.mcmeta"), "w") as file:
+        file.write("""{\n\x20\x20"pack":\x20{\n\t\t"pack_format":\x206,\n\t\t"description":\x20"§eGive §eit §ea §eStar §6https://github.com/gamersindo1223/video-to-minecraft-particles"\n\x20\x20}\n}""")
     with open(dirpath + "/data/minecraft/tags/functions/load.json", "w") as file:
         file.write('{\n\t"values": ["'+f"{function_name}:load"+'"]\n}')
     with open(dirpath + "/data/minecraft/tags/functions/tick.json", "w") as file:
@@ -27,6 +29,6 @@ def make_datapack(function_name, datapack_location):
     }
 
     for file_name, content in file_contents.items():
-        file_path = os.path.join(dirpath + f"/data/{function_name}/functions/", file_name)
+        file_path = os.path.join(dirpath, 'data', function_name, 'functions', file_name)
         with open(file_path, 'w') as file:
             file.write(content)
