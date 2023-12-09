f4e026a30975bf2bf554a869f921ea07e748d6a1
SturdyCobble <jeawon135@gmail.com>
2023-12-09 19:49:01 +0900

    Update main.py

--- lib/main.py
+++ lib/main.py
@@ -3,13 +3,14 @@ import os
 from moviepy.editor import *
 from lib.convert import convert, convert_audio
 from lib.make_folders import make_datapack
-datapack_location = f'{os.getcwd()}/particles-datapack'
-resourcepack_location = f'{os.getcwd()}/audio-packs'
-def main(frames, total_frames, question_answer, filelocation):
+datapack_location = os.path.join(os.getcwd(), 'result', 'particles_datapack')
+resourcepack_location = os.path.join(os.getcwd(), 'result', 'audio_packs')
+def main(frames, total_frames, question_answer, filelocation, fps):
     os.system('cls' if os.name == 'nt' else 'clear')
-    function_name = question_answer['function-name']
+    function_name = "particles" #question_answer['function-name']
     make_datapack(function_name, datapack_location)
-    convert(frames, total_frames, function_name, question_answer['particles-density'], datapack_location)
+    pos0 = (question_answer['x'], question_answer['y'], question_answer['z'])
+    convert(frames, total_frames, function_name, question_answer['particles-density'], datapack_location, fps, pos0)
     convert_audio(filelocation, function_name, resourcepack_location)
     os.system('cls' if os.name == 'nt' else 'clear')
     
